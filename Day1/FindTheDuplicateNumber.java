80bee4dca3cc988782620644ec3ce0623c0bd7fe
yuvarajgoud <yuvarajgoud20@gmail.com>
2024-07-16 19:30:14 +0530

    First Problem

---
+++ Day1/FindTheDuplicateNumber.java
@@ -0,0 +1,24 @@
+class Solution {
+  public int findDuplicate(int[] nums) {
+      int i = 0;
+      int n = nums.length;
+      while(i<n){
+
+          if(nums[i]!=i+1){
+              int correctIndex = nums[i]-1;
+
+              if(nums[i]!=nums[correctIndex]){
+                  int temp = nums[i];
+                  nums[i] = nums[correctIndex];
+                  nums[correctIndex] = temp;
+              } else {
+                  return nums[i];
+              }
+          } else {
+              i++;
+          }    
+      }
+      return -1;
+  }
+  
+}
